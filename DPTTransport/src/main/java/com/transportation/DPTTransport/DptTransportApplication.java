df89e1f8d9027f8eef7f1069b82ab87e31809c5a
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-25 00:29:35 +0100

    add spring boot initial files

---
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/DptTransportApplication.java
@@ -0,0 +1,13 @@
+package com.transportation.DPTTransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DptTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DptTransportApplication.class, args);
+	}
+
+}
---
+++ DPTTransport/src/test/java/com/transportation/DPTTransport/DptTransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transportation.DPTTransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DptTransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
