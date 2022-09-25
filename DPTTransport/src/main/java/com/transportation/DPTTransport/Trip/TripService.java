a47713d168cea5554cccac14ce3d4f48c31c48d4
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-25 16:23:04 +0100

    add pagination to the getListYellow method in the trip service class

--- DPTTransport/src/main/java/com/transportation/DPTTransport/Trip/TripService.java
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/Trip/TripService.java
@@ -1,10 +1,16 @@
 package com.transportation.DPTTransport.Trip;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class TripService {
     private final TripRepository tripRepository;
+
+    public Page<Trip> getListYellow(PageRequest pageRequest){
+        return tripRepository.findAll(pageRequest);
+    }
 }
