3ef26942eacd07472b928c0756380d882e170448
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-25 14:57:39 +0100

    add the zoneTrips class to handle the zone-trips response

---
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/Location/ZoneTrips.java
@@ -0,0 +1,10 @@
+package com.transportation.DPTTransport.Location;
+
+import java.time.LocalDate;
+
+public interface ZoneTrips {
+    String getZone();
+    LocalDate getDate();
+    Long getPu();
+    Long getDo();
+}
