c0b61865dcfd124a541f49d180be72b772853e08
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-25 16:24:08 +0100

    add pagination to the getListYellow method in the trip controller class

--- DPTTransport/src/main/java/com/transportation/DPTTransport/Trip/TripController.java
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/Trip/TripController.java
@@ -1,11 +1,12 @@
 package com.transportation.DPTTransport.Trip;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/trip")
@@ -14,7 +15,8 @@ public class TripController {
     private final TripService tripService;
 
     @GetMapping(path = "list-yellow")
-    public List<Object> getListYellow(){
-        return null;
+    public Page<Trip> getListYellow(@RequestParam int page, @RequestParam int size){
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return tripService.getListYellow(pageRequest);
     }
 }
