aca66f3160170fbf825071a49ea260c3be41194f
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-25 14:11:33 +0100

    add location service class

---
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationService.java
@@ -0,0 +1,10 @@
+package com.transportation.DPTTransport.Location;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class LocationService {
+    
+}
