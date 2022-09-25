3ab188dc24fc7a4ec6cde5a932794b9e7f653b6a
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-25 14:58:33 +0100

    add the getZoneTrips to the location repository class

--- DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationRepository.java
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -14,4 +15,7 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
 
     @Query(value = "select t1.zone, t1.puTotal, t2.doTotal from (select l.zone, count(l.id) as puTotal from location l join trip t on l.id = t.pickup_location_id group by l.zone) as t1 join (select l.zone, count(l.id) as doTotal from location l join trip t on l.id = t.dropoff_location_id group by l.zone) as t2 on t1.zone = t2.zone order by t2.doTotal desc" , nativeQuery = true)
     public List<TopZone> getTopZonesByDropOffs(Pageable pageable);
+
+    @Query()
+    public List<ZoneTrips> getZoneTrips(Long zoneId, LocalDate date);
 }
