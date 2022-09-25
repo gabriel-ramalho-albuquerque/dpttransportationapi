52651b3a91593a081679e5f2de9bb11881ccec18
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-25 14:14:14 +0100

    add location service reference inside the location controller class

--- DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationController.java
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationController.java
@@ -8,5 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/location")
 @AllArgsConstructor
 public class LocationController {
-    
+
+    private final LocationService locationService;
 }
