4320856280f60272af831f14c3ea36f33a89d987
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-25 14:09:03 +0100

    add location controller class

---
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationController.java
@@ -0,0 +1,12 @@
+package com.transportation.DPTTransport.Location;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/location")
+@AllArgsConstructor
+public class LocationController {
+    
+}
