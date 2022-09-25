7b92c6a10d11664319e286a1e42d65dc2b8070de
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-25 14:20:39 +0100

    add the method structure in the controller and service to retrieve the top zones

--- DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationController.java
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationController.java
@@ -1,8 +1,12 @@
 package com.transportation.DPTTransport.Location;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/location")
@@ -10,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class LocationController {
 
     private final LocationService locationService;
+
+    @GetMapping(path = "top-zones")
+    public List<Object> getTopZones(@RequestParam("order") String order){
+        return locationService.getTopZones(order);
+    }
 }
--- DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationService.java
+++ DPTTransport/src/main/java/com/transportation/DPTTransport/Location/LocationService.java
@@ -1,10 +1,15 @@
 package com.transportation.DPTTransport.Location;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class LocationService {
-    
+    public List<Object> getTopZones(String order){
+        return null;
+    }
 }
